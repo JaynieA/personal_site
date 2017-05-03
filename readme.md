ed113c8f348745463eaf29402efce18e37011849
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-05-02 20:26:50 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Description
+
+My personal website and portfolio.
+
+# Technologies Used:
+
+* JavaScript (ES6)
+* React
+* Node.js
+* Webpack 2
+* Babel
+* PostgreSQL
+* HTML5
+* SASS
+
+## Installation
+
+To run locally, `npm install`, then `npm start`
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+let config = {
+  entry:'./app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {test:  /\.jsx?$/, use: ['babel-loader']},
+      {test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
+
+//If we are building for production
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env' : {
+        'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
